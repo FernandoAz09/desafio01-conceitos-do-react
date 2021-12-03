620f6e5e1e4986475148f248a039242b4cf5232e
Fernando Azevedo <azevedo-fernando@users.noreply.github.com>
2021-12-03 20:07:19 -0300

    :tada: Desafio Completo, comentado e testado :tada:

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,37 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    // não permita criar caso o título seja vazio[✔️].
+    if (!newTaskTitle) return; // se falso ,ou seja, vazio, não crie task com valor vazio
+
+    // Crie uma nova task com um id random[✔️]
+    const newTask = { // estado temporário
+        id: Math.random(), // gerar um numero aleatório
+        title: newTaskTitle,
+        isComplete: false // começa como falso para poder trocar depois para completo
+    }
+
+    setTasks(oldState => [...oldState, newTask]); // Adicionar à listagem 
+    // valor antigo (oldState) ... spread operator, pegar todos os valores usados anteriormente, e colocar novo texto com o newTask
+
+    setNewTaskTitle('') // deixar o campo vazio após criar nova task
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID[✔️]
+    const newTasks = tasks.map(task => task.id === id ? { // mapear todas as tasks, pegar a task que tem o mesmo id, se for igual, editar isComplete.
+        ...task,
+        isComplete: !task.isComplete // pegar o valor antigo (false) e sobrescrever como (true)
+    } : task) // ou se o task.id for igual ao id, retorna a mesma task, não alterando o estado
+
+    setTasks(newTasks) // alterando o estado com um novo estado, alterando o isComplete de false para true
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    // Remova uma task da listagem pelo ID [✔️]
+    const filteredTasks = tasks.filter(task => task.id !== id) // filtrar no array a task com a task.id diferente dos id já existentes 
+
+    setTasks(filteredTasks) // alterando o estado com um novo estado, com os tasks.id diferentes do que foi clicado
   }
 
   return (
@@ -36,7 +58,7 @@ export function TaskList() {
             type="text" 
             placeholder="Adicionar novo todo" 
             onChange={(e) => setNewTaskTitle(e.target.value)}
-            value={newTaskTitle}
+            value={newTaskTitle} //componentes controlados pelo react
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
             <FiCheckSquare size={16} color="#fff"/>
